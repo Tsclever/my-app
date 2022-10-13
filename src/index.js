958d50933b19415427c147c9920b08dd7064ee89
Tsclever <yxt15850005248@outlook.com>
2022-10-13 19:04:06 +0800

    修改项目结构，把组件归入文件夹

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import Hw from './Hw';
 import Photo from './MyFirstComponent';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Hw />
     <Photo />
     <Photo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
