7af6ca6c8ea90695723c6ddae00887969cf535cd
Tsclever <yxt15850005248@outlook.com>
2022-11-14 21:00:02 +0800

    在 Signup 组件中，实现 使用 preventDefault() 函数，来阻止默认事件的发生，点击 send 不会提示 “Submitting”

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -2,9 +2,14 @@ function Signup() {
   return (
     <form onSubmit={() => alert('Submitting!')}>
       <input />
-      <button>Send</button>
+      <button onClick={e => e.preventDefault()}>Send</button>
     </form>
   );
 }
 
 export default Signup;
+
+// 修改完毕后，再怎么去点击页面中的 button，也不会触发 onSubmit 里的代码了。最后请注意，不要混淆`e.stopPropagation()`和`e.preventDefault()`。它们被用来阻止不必要的代码触发，但并不相同：
+
+// -  [`e.stopPropagation()`](https://developer.mozilla.org/docs/Web/API/Event/stopPropagation) 停止触发附加到标签的 event handler。
+// -  [`e.preventDefault()`](https://developer.mozilla.org/docs/Web/API/Event/preventDefault) 阻止只有少数标签拥有的默认的浏览器事件。
